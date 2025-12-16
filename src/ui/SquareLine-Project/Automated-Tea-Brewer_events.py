4f13a65b4dc4840bbb9cf85c3ea6b4b69219dddc
tadeastehan <tadeastehan@gmail.com>
2025-12-16 21:38:09 +0100

    Add initial implementation of Automated Tea Brewer event handlers and project assets
    - Created event handler functions for various screens in Automated Tea Brewer.
    - Added a default theme configuration in Themes.slt.
    - Included multiple asset images for the user interface, such as tea screens and icons.
    - Initialized project metadata in project.info file.

---
+++ src/ui/SquareLine-Project/Automated-Tea-Brewer_events.py
@@ -0,0 +1,96 @@
+
+def nextTeaScreen(event_struct):
+    return
+
+
+def onMainScreen(event_struct):
+    return
+
+
+def onTeaScreen(event_struct):
+    return
+
+
+def onTeapotScreen(event_struct):
+    return
+
+
+def onBrewInfuseScreen(event_struct):
+    return
+
+
+def stopBrewing(event_struct):
+    return
+
+
+def onErrorScreen(event_struct):
+    return
+
+
+def onTeaPropertieScreen(event_struct):
+    return
+
+
+def changeBrewingTeaTemperature(event_struct):
+    return
+
+
+def changeInfusionTimeSecond(event_struct):
+    return
+
+
+def changeInfusionTimeMinute(event_struct):
+    return
+
+
+def onBrewNowOrSchedulerScreen(event_struct):
+    return
+
+
+def brewNow(event_struct):
+    return
+
+
+def onSettingsScreen(event_struct):
+    return
+
+
+def changeDryingTime(event_struct):
+    return
+
+
+def dryingPositionUp(event_struct):
+    return
+
+
+def dryingPositionDown(event_struct):
+    return
+
+
+def onSchedulerScreen(event_struct):
+    return
+
+
+def changeSchedulerTemperature(event_struct):
+    return
+
+
+def changeSchedulerTimeMinute(event_struct):
+    return
+
+
+def changeSchedulerTimeHour(event_struct):
+    return
+
+
+def beginScheduler(event_struct):
+    return
+
+
+def onScheduledScreen(event_struct):
+    return
+
+
+def stopScheduledBrew(event_struct):
+    return
+
